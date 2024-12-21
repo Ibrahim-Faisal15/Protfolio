4648f40087183c189a4e58c03a9eda34b756481b
BeastKing <139386713+Ibrahim-Faisal15@users.noreply.github.com>
2024-12-21 14:08:17 +0500

    *

--- src/components/Main_.jsx
+++ src/components/Main_.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 
 function Main() {
+  const imgUrl = new URL("../../public/portfolio_photo.jpg", import.meta.url).href;
   return (
     <div className="details  flex  flex-col h-[35%] w-[45%] sm:h-[50%] sm:w-[45%]  md:w-[30%] md:h-[35%] lg:w-[20%] lg:h-[50%]  border-2 border-white   ">
       <div className="picture flex justify-center items-center  h-[50%] w-[100%] ">
         <img
-          src="../../public/portfolio_photo.jpg"
+          src={imgUrl}
           alt="Avatar"
           className="rounded-full h-[6.25rem] w-[6.25rem] sm:h-[9rem] sm:w-[9rem] md:h-[7rem] md:w-[7rem] lg:h-[10rem] lg:w-[10rem] "
         />
